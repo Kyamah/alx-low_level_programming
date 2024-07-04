126a245fd1f53adc67bc1d24830a92b8ce81b0ef
Kyamah <kyamah10@gmail.com>
2024-07-04 01:27:23 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void _putchar(char c);
+void _putchar(char c) {
+    putchar(c);
 
 #endif
